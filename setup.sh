4243a2baa701bfbfa5cd225bf2ee85f86ae9b95e
johnnyzheng0636 <johnnyzheng0636@gmail.com>
2025-08-04 22:46:11 +0800

    init

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ pip3 install torch torchvision torchaudio --index-url https://download.pytorch.o
 git clone --depth 1 https://github.com/EleutherAI/lm-evaluation-harness
 cd lm-evaluation-harness
 pip install -e .
-pip install numpy datasets matplotlib psutil protobuf transformers==4.51.3
+pip install numpy datasets matplotlib psutil protobuf transformers==4.50.3
